a89486e47b0c33c0559d0ae9ec8db1eaa9705ac0
Marco Quintella <2om.marco.quintella@gmail.com>
2024-08-26 12:57:44 -0300

    chore: install unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@unocss/nuxt',
+  ],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
